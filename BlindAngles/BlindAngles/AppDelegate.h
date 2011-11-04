cfcdb4b6ab5273495ed578d378ee9e4a832de232
Robert Xiao <brx@cs.cmu.edu>
2011-11-04 15:33:24 -0400

    fixing names

--- BlindAngles/BlindAngles/AppDelegate.h
+++ BlindAngles/BlindAngles/AppDelegate.h
@@ -2,8 +2,8 @@
 //  AppDelegate.h
 //  BlindAngles
 //
-//  Created by Bo Xiao on 11/4/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Created by Robert Xiao on 11/4/11.
+//  Copyright (c) 2011 Human-Computer Interaction Institute. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
