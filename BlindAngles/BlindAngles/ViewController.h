cac85acc1e9d58634ade55529195130698f5a68c
Robert Xiao <brx@cs.cmu.edu>
2011-11-04 15:09:43 -0400

    Create new BlindAngles project using single view template

---
+++ BlindAngles/BlindAngles/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BlindAngles
+//
+//  Created by Bo Xiao on 11/4/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
