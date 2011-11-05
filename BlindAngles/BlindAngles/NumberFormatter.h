f52148b5c6e6daba2f1f8728599bc2e3abeb6812
Robert Xiao <brx@cs.cmu.edu>
2011-11-05 16:14:08 -0400

    Implement NumberFormatter methods

--- BlindAngles/BlindAngles/NumberFormatter.h
+++ BlindAngles/BlindAngles/NumberFormatter.h
@@ -10,4 +10,6 @@
 
 @interface NumberFormatter : NSObject
 
++ (NSString *)getAccessibilityLabelForAngleLabel:(NSString *)labelText;
+
 @end
--- BlindAngles/BlindAngles/NumberFormatter.m
+++ BlindAngles/BlindAngles/NumberFormatter.m
@@ -10,4 +10,22 @@
 
 @implementation NumberFormatter
 
++ (NSString *)getAccessibilityLabelForAngleLabel:(NSString *)labelText {
+    if(labelText.floatValue == 1.0) {
+        return @"1 degree";
+    } else {
+        if([labelText length] == 0) {
+            NSLog(@"Invalid label text received...");
+            return @"0 degrees";
+        }
+        if([labelText characterAtIndex:[labelText length] - 1] == '.') {
+            /* Strip off trailing dot for accessibility label */
+            labelText = [labelText substringToIndex:[labelText length] - 1];
+        }
+        /* Replace "." (spoken as 'dot') with " point " */
+        labelText = [labelText stringByReplacingOccurrencesOfString:@"." withString:@" point "];
+        return [labelText stringByAppendingString:@" degrees"];
+    }
+}
+
 @end
