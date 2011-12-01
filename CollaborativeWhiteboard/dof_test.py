4a723c7504681663a022dd3bfa50b08e06eb0e63
Robert Xiao <brx@cs.cmu.edu>
2011-12-01 17:18:48 -0500

    Apply Sauvik's downsampling suggestion to increase speed 10 fold

--- CollaborativeWhiteboard/dof_test.py
+++ CollaborativeWhiteboard/dof_test.py
@@ -2,7 +2,7 @@
 import pygame
 import freenect
 import numpy as np
-from scipy import ndimage
+from scipy.misc import pilutil
 
 def dof_buckets(depth):
     ''' return array masks corresponding to different depths '''
@@ -11,25 +11,24 @@ def dof_buckets(depth):
     for i, stop in enumerate((600, 1000, 1400, 1800, 2200, 2600, 3000, 65535)):
         mask = depth < stop
         # Blur amount is (currently) just set to mask level
-        masks.append((i*4, mask & ~prev))
+        masks.append((i*4+1, mask & ~prev))
         prev = mask
     return masks
 
 def dof_filter(channel, masks):
     ''' filter image `channel' using dof masks '''
     accum = np.zeros_like(channel)
-    temp = np.empty_like(channel)
     for sigma, mask in masks:
-        blurred = ndimage.filters.gaussian_filter(channel * mask,
-            sigma=sigma, output=temp, mode='reflect')
-        np.add(accum, temp, accum)
+        w, h = channel.shape
+        small = pilutil.imresize(channel, (w//sigma, h//sigma))
+        blurred = pilutil.imresize(small, (w, h))
+        np.add(accum, blurred * mask, accum)
     return accum
 
 def to_surfimage(r, g, b):
-    print np.max(r)
-    r = r.astype('uint8').astype('uint32') << 24
-    g = g.astype('uint8').astype('uint32') << 16
-    b = b.astype('uint8').astype('uint32') << 8
+    r = r.astype('uint32') << 24
+    g = g.astype('uint32') << 16
+    b = b.astype('uint32') << 8
     return (r + g + b + 0xff)
 
 if __name__ == '__main__':
@@ -52,9 +51,9 @@ if __name__ == '__main__':
         masks = dof_buckets(depth)
 
         video, video_timestamp = freenect.sync_get_video(format=freenect.VIDEO_RGB)
-        r = dof_filter(video[..., 2].astype('float32'), masks)
-        g = dof_filter(video[..., 1].astype('float32'), masks)
-        b = dof_filter(video[..., 0].astype('float32'), masks)
+        r = dof_filter(video[..., 2], masks)
+        g = dof_filter(video[..., 1], masks)
+        b = dof_filter(video[..., 0], masks)
         sa[:640, :480] = to_surfimage(r, g, b).transpose()
 
         del sa
